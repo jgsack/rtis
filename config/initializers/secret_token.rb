f6aa7004d0ee847709d61169b7a694d7fd36dc5d
J G Sack/n <jgsack@gmail.com/n>
2013-08-09 10:54:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rtipost::Application.config.secret_key_base = 'becfd76ce8f70ca5d6816242a735dbaede6373b76674a58ac9787c241b6eced7d13a447c635e9af1a33acf6275ee462b8c36eff3420b7557b36768858ee736ac'
